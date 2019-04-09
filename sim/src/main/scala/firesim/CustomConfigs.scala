259f0d57be0e60ba464c99bba83beced1a6770a3
Farzad Farshchi <farzadfr@gmail.com>
2019-04-09 21:01:54 +0000

    Add a new scala file to hold the custom platform and target configs

---
+++ sim/src/main/scala/firesim/CustomConfigs.scala
@@ -0,0 +1,23 @@
+package firesim.firesim
+
+import freechips.rocketchip.config.Config
+import freechips.rocketchip.subsystem._
+import boom.system.BoomTilesKey
+import midas.models._
+
+// Custom SimConfigs
+class With20MSHRs extends Config((site, here, up) => {
+  case LlcKey => up(LlcKey, site).map(_.copy(mshrs = WRange(1, 20)))
+})
+
+// Custom TargetConfigs
+class With5IdBits extends Config((site, here, up) => {
+  case ExtMem => up(ExtMem, site).map(_.copy(idBits = 5))
+})
+
+class With16kL1Booms extends Config((site, here, up) => {
+  case BoomTilesKey => up(BoomTilesKey, site) map { r =>r.copy(
+    dcache = Some(r.dcache.get.copy(nWays=4)),
+    icache = Some(r.icache.get.copy(nWays=4))
+  )}
+})
